5ac4e771202aa96a387839e49ab225c74c2c990c
Shanto Islam <ishanto412@gmail.com>
2024-10-21 14:43:33 +0600

    fix build problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@/components/theme-provider';
 import Routers from './routes';
 import { AuthProvider } from '@/contexts/AuthProvider';
+import { Toaster } from '@/components/ui/sonner';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         storageKey="t2site-theme"
       >
         <Routers />
+        <Toaster />
       </ThemeProvider>
     </AuthProvider>
   );
--- src/contexts/AuthProvider.tsx
+++ src/contexts/AuthProvider.tsx
@@ -1,3 +1,4 @@
+import { delay } from '@/lib/utils';
 import {
   createContext,
   useContext,
@@ -5,6 +6,7 @@ import {
   ReactNode,
   useEffect
 } from 'react';
+import { toast } from 'sonner';
 
 interface AuthContextType {
   isAuthenticated: boolean;
@@ -36,14 +38,25 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     localStorage.setItem('isAuthenticated', JSON.stringify(isAuthenticated));
   }, [isAuthenticated]);
 
-  const login = (email: string, password: string) => {
+  const login = async (email: string, password: string) => {
     setIsAuthenticated(true);
     console.log('User logged in:', email, password);
   };
 
   const logout = () => {
-    setIsAuthenticated(false);
-    console.log('User logged out');
+    toast.promise(delay(500), {
+      loading: 'Singing in...',
+      success: () => {
+        setIsAuthenticated(false);
+        return `You have loged out!!`;
+      },
+      finally: () => {
+        console.log('User logged out');
+      },
+      position: 'top-center',
+      duration: 1000,
+      error: () => 'Signed out failed !!'
+    });
   };
 
   return (
