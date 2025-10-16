4dbae4871032c30284649c503a34605f236ec7ea
Kawsar Ahmed <kawsarahmed130@gmail.com>
2025-10-16 12:02:46 +0600

    fix: Input focus color updated to align with the Primary color

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -16,8 +16,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           'transition-shadow duration-200',
           'placeholder:text-muted-foreground',
           'file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground focus-visible:outline-none',
-          'dark:focus-visible:shadow-[0_0_0_1px_rgba(234,179,8,0.3),0_0_2px_4px_rgba(234,179,8,0.15)]',
-          'focus-visible:shadow-[0_0_0_1px_rgb(255_206_52_/_46%),_0_0_1px_3px_rgb(234_179_8_/_24%)]',
+          'focus-visible:shadow-[0_0_0_1px_rgb(0_0_0_/_46%),_0_0_1px_3px_rgb(0_0_0_/_24%)]',
           'disabled:cursor-not-allowed disabled:opacity-50',
           className
         )}
