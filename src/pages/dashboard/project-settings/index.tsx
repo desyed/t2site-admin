e8b3b9cdfa045ef83c7fee31bda7b12e4fb5ff4b
Kawsar Ahmed <kawsarahmed130@gmail.com>
2025-11-15 07:01:44 +0600

    fix: FAQ Form file name updated

--- src/pages/dashboard/project-settings/index.tsx
+++ src/pages/dashboard/project-settings/index.tsx
@@ -8,7 +8,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { createDashboardLoader } from '@/middlewares/auth-middleware';
 
-import { FaqForm } from './general/_components/FaqForm';
+import { FaqForm } from './general/_components/faq-form';
 
 export const loader = createDashboardLoader(() => {
   return {
