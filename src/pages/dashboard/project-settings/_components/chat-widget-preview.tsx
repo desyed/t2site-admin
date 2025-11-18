cf4acb07ae9423eb85eee05a54d9193e60b5a4be
Kawsar Ahmed <kawsarahmed130@gmail.com>
2025-11-18 09:36:50 +0600

    fix: Multiple Chat Preview screen added with React Slick Slider

--- src/pages/dashboard/project-settings/_components/chat-widget-preview.tsx
+++ src/pages/dashboard/project-settings/_components/chat-widget-preview.tsx
@@ -1,4 +1,5 @@
 import { Plus, Triangle, HelpCircle } from 'lucide-react';
+import Slider from 'react-slick';
 
 import { MessageIcon } from '@/components/icons';
 import { Button } from '@/components/site-button';
@@ -11,73 +12,154 @@ interface ChatWidgetPreviewProps {
   logoPreviewUrl: string;
 }
 
+const sliderSettings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
+
 const ChatWidgetPreview = ({ logoPreviewUrl }: ChatWidgetPreviewProps) => {
   return (
-    <div className="w-[400px] overflow-hidden rounded-xl border bg-gradient-to-b from-[var(--chat-bg)] to-[#F5F5F5] shadow">
-      {/* Chat Header */}
-      <div className="flex items-center justify-between px-6 py-4 text-primary">
-        <div className="flex items-center gap-2.5">
-          <div
-            className={`flex size-11 items-center justify-center rounded-full bg-[var(--chat-badge)]`}
-          >
-            <div className="size-3/5">
-              {logoPreviewUrl ? (
-                <img
-                  src={logoPreviewUrl}
-                  alt="logo"
-                  className="size-full object-contain"
+    <div className="h-[600px] w-[400px] rounded-xl outline outline-1 outline-gray-200">
+      <Slider {...sliderSettings} className="h-full">
+        {/* Conversation Screen */}
+        <div className="h-[600px] overflow-hidden rounded-xl bg-gradient-to-b from-[var(--chat-bg)] to-[#F5F5F5]">
+          {/* Chat Header */}
+          <div className="flex h-[100px] items-center justify-between px-6 py-4 text-primary">
+            <div className="flex items-center gap-2.5">
+              <div
+                className={`flex size-11 items-center justify-center rounded-full bg-[var(--chat-badge)]`}
+              >
+                <div className="size-3/5">
+                  {logoPreviewUrl ? (
+                    <img
+                      src={logoPreviewUrl}
+                      alt="logo"
+                      className="size-full object-contain"
+                    />
+                  ) : (
+                    <MessageIcon />
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Chat Conversation */}
+          <div className="h-[424px] px-2">
+            <div className="rounded-t-xl bg-[#F5F5F5] p-4">
+              <ChatConversation logoPreviewUrl={logoPreviewUrl} />
+            </div>
+          </div>
+
+          {/* Chat Toolbar */}
+          <div className="h-[76px] bg-[#E6E6E6] pb-2 pt-4">
+            <div className="flex items-center gap-2 px-2 pb-1 sm:px-3 sm:pb-2">
+              <div className="flex w-full items-center gap-2 overflow-hidden rounded-[28px] border bg-background">
+                <div className={cn('pl-2', 'order-1 w-fit')}>
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    className={cn(
+                      'size-8 rounded-full text-muted-foreground hover:text-foreground'
+                    )}
+                  >
+                    <Plus className="size-4" />
+                  </Button>
+                </div>
+
+                <Input
+                  className="order-2 flex-1 border-none focus-visible:shadow-none focus-visible:outline-none"
+                  placeholder="Write a message..."
+                  readOnly
                 />
-              ) : (
-                <MessageIcon />
-              )}
+
+                <div className="order-3 flex items-center gap-2 px-2">
+                  <Button
+                    size="icon"
+                    className="size-7 gap-2 rounded-full bg-[var(--chat-fg)]"
+                  >
+                    <Triangle className="ml-0.5 size-4 rotate-90" />
+                  </Button>
+                </div>
+              </div>
+              <div>
+                <HelpCircle />
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      {/* Chat Conversation */}
-      <div className="px-2">
-        <div className="rounded-t-xl bg-[#F5F5F5] p-4">
-          <ChatConversation logoPreviewUrl={logoPreviewUrl} />
-        </div>
-      </div>
-
-      {/* Chat Toolbar */}
-      <div className="bg-[#E6E6E6] pb-2 pt-4">
-        <div className="flex items-center gap-2 px-2 pb-1 sm:px-3 sm:pb-2">
-          <div className="flex w-full items-center gap-2 overflow-hidden rounded-[28px] border bg-background">
-            <div className={cn('pl-2', 'order-1 w-fit')}>
-              <Button
-                variant="ghost"
-                size="icon"
-                className={cn(
-                  'size-8 rounded-full text-muted-foreground hover:text-foreground'
-                )}
+        {/* Initial Screen */}
+        <div className="h-[600px] overflow-hidden rounded-xl bg-gradient-to-b from-[var(--chat-bg)] to-[#F5F5F5]">
+          {/* Chat Header */}
+          <div className="flex h-[100px] items-center justify-between px-6 py-4 text-primary">
+            <div className="flex items-center gap-2.5">
+              <div
+                className={`flex size-11 items-center justify-center rounded-full bg-[var(--chat-badge)]`}
               >
-                <Plus className="size-4" />
-              </Button>
-            </div>
-
-            <Input
-              className="order-2 flex-1 border-none focus-visible:shadow-none focus-visible:outline-none"
-              placeholder="Write a message..."
-              readOnly
-            />
-
-            <div className="order-3 flex items-center gap-2 px-2">
-              <Button
-                size="icon"
-                className="size-7 gap-2 rounded-full bg-[var(--chat-fg)]"
-              >
-                <Triangle className="ml-0.5 size-4 rotate-90" />
-              </Button>
+                <div className="size-3/5">
+                  {logoPreviewUrl ? (
+                    <img
+                      src={logoPreviewUrl}
+                      alt="logo"
+                      className="size-full object-contain"
+                    />
+                  ) : (
+                    <MessageIcon />
+                  )}
+                </div>
+              </div>
             </div>
           </div>
-          <div>
-            <HelpCircle />
+
+          {/* Chat Conversation */}
+          <div className="h-[424px] px-2">
+            <div className="rounded-t-xl bg-[#F5F5F5] p-4">
+              Promotional CTA and Banner
+            </div>
+          </div>
+
+          {/* Chat Toolbar */}
+          <div className="h-[76px] bg-[#E6E6E6] pb-2 pt-4">
+            <div className="flex items-center gap-2 px-2 pb-1 sm:px-3 sm:pb-2">
+              <div className="flex w-full items-center gap-2 overflow-hidden rounded-[28px] border bg-background">
+                <div className={cn('pl-2', 'order-1 w-fit')}>
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    className={cn(
+                      'size-8 rounded-full text-muted-foreground hover:text-foreground'
+                    )}
+                  >
+                    <Plus className="size-4" />
+                  </Button>
+                </div>
+
+                <Input
+                  className="order-2 flex-1 border-none focus-visible:shadow-none focus-visible:outline-none"
+                  placeholder="Write a message..."
+                  readOnly
+                />
+
+                <div className="order-3 flex items-center gap-2 px-2">
+                  <Button
+                    size="icon"
+                    className="size-7 gap-2 rounded-full bg-[var(--chat-fg)]"
+                  >
+                    <Triangle className="ml-0.5 size-4 rotate-90" />
+                  </Button>
+                </div>
+              </div>
+              <div>
+                <HelpCircle />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </Slider>
     </div>
   );
 };
